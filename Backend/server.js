7db70200bab084b248489fc9637ea55adf467e22
Bhagya Lakshmi <bhagyaa@elberttech.com>
2025-12-09 18:08:07 +0530

    initial deploy

--- Backend/server.js
+++ Backend/server.js
@@ -2,19 +2,20 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect DB
 connectDB();
 
-// Routes
 app.use("/api/companies", require("./routes/companyRoutes"));
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
